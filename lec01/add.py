dccd32eb936d3f9af884be65c7e974442e225450
zj <zj@webturing.com>
2019-10-10 20:35:46 +0800

    lec01

---
+++ lec01/add.py
@@ -0,0 +1,3 @@
+s=input()
+s=int(s)
+print(s+s)
---
+++ lec01/table.py
@@ -0,0 +1,18 @@
+# print a table
+x=1
+print(x,x*x,x**3)
+x=x+1
+print(x,x*x,x**3)
+x=x+1
+
+print(x,x*x,x**3)
+x=x+1
+
+print(x,x*x,x**3)
+x=x+1
+
+print(x,x*x,x**3)
+x=x+1
+
+print(x,x*x,x**3)
+x=x+1
