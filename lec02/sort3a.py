a46992f29e23b0185fa446055af5c4f10a97a331
zj <zj@webturing>
2019-10-12 11:58:28 +0800

    chains comparision

--- lec02/sort3a.py
+++ lec02/sort3a.py
@@ -1,13 +1,13 @@
 a, b, c = map(int, input().strip().split())
-if a <= b and b <= c:
+if a <= b <= c:
     print(a, b, c)
-elif a <= c and c <= b:
+elif a <= c <= b:
     print(a, c, b)
-elif b <= a and a <= c:
+elif b <= a <= c:
     print(b, a, c)
-elif b <= c and c <= a:
+elif b <= c <= a:
     print(b, c, a)
-elif c <= a and a <= b:
+elif c <= a <= b:
     print(c, a, b)
 else:
     print(c, b, a)
