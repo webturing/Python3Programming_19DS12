68b3e48c8b28bb1b67501e754cf802cd2db5ad7b
zj <zj@webturing.com>
2019-10-18 15:50:13 +0800

    lec04

---
+++ lec04/E.py
@@ -0,0 +1,14 @@
+while True:
+    a=int(input())
+    if a<0 or a>100:
+        print('Score is error!')
+    elif a<60:
+        print('E')
+    elif a<70:
+        print('D')
+    elif a<80:
+        print('C')
+    elif a<90:
+        print('B')
+    else:
+        print('A')
---
+++ lec04/flower.py
@@ -0,0 +1,11 @@
+for n in range(100,1000):
+    c = n % 10
+    b = (n // 10) % 10
+    a = (n // 100)
+    # print(a,b,c)
+    m = a ** 3 + b ** 3 + c ** 3
+    # print('m=',m)
+    if m == n:
+        print(m)
+
+
