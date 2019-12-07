92c52672d977f87d8f26a1d762cffe9e44611cf3
webturing <zj@webturing.com>
2019-12-07 11:56:59 +0800

    lec18文件读

--- lec18file1/decode.py
+++ lec18file1/decode.py
@@ -1,9 +1,10 @@
 K = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
 with open("code.txt") as myFile:
     text = myFile.readline()
     for offset in range(1, 26):
-
+      
         for char in text:
             if char not in K:
                 print(char, end="")
