4d10376719911c136a0bb7dd0a16b52ed0b3e7f8
webturing <zj@webturing.com>
2019-12-10 23:45:46 +0800

    lec 20 python oop

---
+++ lec20class/Fraction.py
@@ -0,0 +1,28 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+
+class Fraction:
+    def __init__(self, up=0, down=1):
+        g = gcd(up, down)
+        up, down = up // g, down // g
+        self.up, self.down = up, down
+
+    def __str__(self): return '%d/%d' % (self.up, self.down)
+
+    def __add__(self, other):
+        up, down = self.up * other.down + self.down * other.up, self.down * other.down
+        return Fraction(up, down)
+
+
+print(Fraction())
+print(Fraction(2))
+print(Fraction(3, 6))
+print(Fraction(1, 3) + Fraction(1, 6))
+s = Fraction()
+for i in range(1, 11):
+    s += Fraction(1, i)
+print(s)
